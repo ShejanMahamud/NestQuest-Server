00118fdddb05c3732ba8d0de6eb0ffdfe819447c
ShejanMahamud <mdshejanmahamud006@gmail.com>
2024-06-02 01:05:42 +0600

    jwt on local storage fixed

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ const app = express();
 //middlewares
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173"
-    ]
+    origin: ["http://localhost:5173"],
   })
 );
 app.use(express.json());
@@ -29,23 +27,40 @@ const client = new MongoClient(mongoURI, {
   },
 });
 
-    // middlewares 
-    const verifyToken = (req, res, next) => {
-      if (!req.headers.authorization) {
-        return res.status(401).send({ message: 'unauthorized access' });
-      }
-      const token = req.headers.authorization.split(' ')[1];
-      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-          return res.status(401).send({ message: 'unauthorized access' })
-        }
-        req.decoded = decoded;
-        next();
-      })
+// middlewares
+const verifyToken = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  jwt.verify(token, secret_token, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
     }
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const run = async () => {
   try {
+    //collections
+    const usersCollection = client.db("nestquest").collection("users");
+
+    //get users from db
+    app.get('/users',async(req,res)=>{
+      const result = await usersCollection.find().toArray()
+      res.send(result)
+    })
+
+
+    //save a user in db in register
+    app.post('/users',async(req,res)=>{
+      const user = req.body;
+      const result = await usersCollection.insertOne(user)
+      res.send(result)
+    })
+
     // jwt related api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
