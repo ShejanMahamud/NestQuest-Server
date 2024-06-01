e15ba79a2bfe56f93004fb6c0de4c7d83cb43ed7
ShejanMahamud <mdshejanmahamud006@gmail.com>
2024-06-01 11:31:48 +0600

    project setup complete

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ObjectId, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 4549;
+const mongoURI = process.env.MONGO_URI;
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+const secret_token = process.env.ACCESS_TOKEN_SECRET;
+
+//app
+const app = express();
+
+//middlewares
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://bookify-library.netlify.app",
+      "https://bookify-library-client.firebaseapp.com",
+    ],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+//mongo client
+const client = new MongoClient(mongoURI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+//custom  middlewares
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  if (!token) {
+    // console.log('token nei')
+    return res.status(401).send({ message: "Forbidden Access!" });
+  }
+  jwt.verify(token, secret_token, (error, decoded) => {
+    if (error) {
+      // console.log('token nosto')
+      return res.status(401).send({ message: "Forbidden Access!" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+//cookies options
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
+const run = async () => {
+  try {
+    // await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+};
+
+run().catch((error) => console.log);
+
+app.get("/", (req, res) => {
+  res.send({ server_status: "Server Running" });
+});
+
+app.listen(port, () => {
+  console.log("Server running on", port);
+});
